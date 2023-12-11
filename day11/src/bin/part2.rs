a72eafe2cae761e940d44bf46e4ac3546ce112a7
Ricardo Cárdenes <ricardo.cardenes@noirlab.edu>
2023-12-11 11:41:41 -0300

    Day 11: minor improvement

--- day11/src/bin/part2.rs
+++ day11/src/bin/part2.rs
@@ -3,6 +3,7 @@ use std::io::{stdin, BufReader};
 use day11::read_map;
 fn main() {
     let map = read_map(BufReader::new(stdin()));
-    let distances = map.expand(1000000).distances();
-    println!("Sum of distances between galaxies (x1000000): {}", distances.iter().sum::<usize>());
+    let factor = 1000000;
+    let distances = map.expand(factor).distances();
+    println!("Sum of distances between galaxies (x{factor}): {}", distances.iter().sum::<usize>());
 }
